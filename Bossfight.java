d120e07ea7f87782d45ee9776be0ee06a83c72f7
TGMi <74232143+TGMi123@users.noreply.github.com>
2022-03-21 11:18:54 +0800

    Add files via upload
    Files

---
+++ Bossfight.java
@@ -0,0 +1,152 @@
+/*
+ * Created by MrCrocodile (Aka TGMi123 / TGMi)
+ * Thanks to Github and Basic Projects Games :):):):)
+ * 
+ * 
+ * This Code since i made when i was 12 Years old
+ * Made in 🇵🇭
+ */
+import java.util.Scanner;
+public class Bossfight {
+    public static void main(String[] args) {
+        //User Input
+        Scanner user = new Scanner (System.in);
+        //Variables
+        int Sword = 7;
+        int RareSword = 4;
+        int UltraSword = 2;
+        int hpPlayer = 20;
+        int shieldPlayer = 30;
+        int hpBossfight = 30;
+        int round = 1;
+        int hpAgain = 1;
+        int SwordPrize = (int)(Math.random() * 20);
+        int RareSwordPrize = (int)(Math.random() * 12);
+        int UltraSwordPrize = (int)(Math.random() * 8);
+        String bossfight;
+        //Speech Bubble
+        user.nextLine();
+        System.out.println("Hello!");
+        user.nextLine();
+        System.out.println("Will you not Responed?");
+        user.nextLine();
+        System.out.println("Fine, your not Responed! I will fight btw uwu\n====================");
+        //Menu
+        System.out.println("Start the Game? type \"Start\" to start");
+        String startgame = user.nextLine();
+        //Game
+        if (startgame.equals("Start")) {
+            System.out.println("=====ROUND 1=====");
+            while (true) {
+                //Lose
+                if (hpPlayer < 1) {
+                    System.out.println("=====GAME OVER=====\n>THANKS TO<\nPotato Chronicler\nNapstaparrot\nU-ndefined\nWELBIC\n>CREATED BY<\nMrCrocodile");
+                    break;
+                }
+                //Win
+                if (hpBossfight < 1) {
+                    //Prizes
+                    SwordPrize = (int)(Math.random() * 20);
+                    RareSwordPrize = (int)(Math.random() * 12);
+                    UltraSwordPrize = (int)(Math.random() * 8);
+                    System.out.println("==YOU DEFEAT THE BOSSFIGHT==\nCollect Rewards!\n" + SwordPrize + " Swords\n" + RareSwordPrize + " Rare Swords\n" + UltraSwordPrize + " Ultra Swords\nType \"Collect\" to get some rewards or type any key to remove the reward");
+                    String reward = user.nextLine();
+                    if (reward.equals("Collect")) {
+                        Sword = Sword + SwordPrize;
+                        UltraSword = UltraSword + UltraSwordPrize;
+                        RareSword = RareSword + RareSwordPrize;
+                        System.out.println("Collected!\nStart Again? type \"Start\" to start");
+                    } else {
+                        System.out.println("Collect canceled...\nStart Again? type \"Start\" to start");
+                    }
+                    //Start Again
+                    String gameagain = user.nextLine();
+                    if (gameagain.equals("Start")) {
+                        System.out.println("Well Start Now!");
+                        hpAgain++;
+                        hpBossfight = 30 * hpAgain;
+                        hpPlayer = 20 * hpAgain;
+                        shieldPlayer = 30 * hpAgain;
+                    } else {
+                        System.out.println("Well the Game Closed...\nCREATED BY MRCROCODILE");
+                        break;
+                    }
+                    round++;
+                    System.out.println("=====ROUND " + round + "=====");
+                }
+                //Inventory
+                System.out.println("---------------\n==WEAPONS==\nSword (4 Damage): " + Sword + "\nRare Sword (6 Damage): " + RareSword + "\nUltra Sword (8 Damage): " + UltraSword + "\n==PLAYER==\nHealth: " + hpPlayer + "\nShield: " + shieldPlayer + "\n==BOSS==\nBoss Health: " + hpBossfight + "\n---------------");
+                String weapon = user.nextLine();
+                if (weapon.equals("Sword")) {
+                    if (Sword > 0) {
+                        --Sword;
+                        hpBossfight = hpBossfight - 4;
+                    } else if (Sword < 0 || RareSword < 0 || UltraSword < 0) {
+                        System.out.println("!: Your All inventory is empty");
+                    } else {
+                        Sword = 0;
+                        System.out.println("!: Your Sword is empty");
+                    }
+                } else if (weapon.equals("Rare Sword")) {
+                    if (RareSword > 0) {
+                        --RareSword;
+                        hpBossfight = hpBossfight - 6;
+                    } else if (Sword < 0 || RareSword < 0 || UltraSword < 0) {
+                        System.out.println("!: Your All inventory is empty");
+                    } else {
+                        RareSword = 0;
+                        System.out.println("!: Your Rare Sword is empty");
+                    }
+                } else if (weapon.equals("Ultra Sword")) {
+                    if (UltraSword > 0) {
+                        --UltraSword;
+                        hpBossfight = hpBossfight - 8;
+                    } else if (Sword < 0 || RareSword < 0 || UltraSword < 0) {
+                        System.out.println("!: Your All inventory is empty");
+                    } else {
+                        UltraSword = 0;
+                        System.out.println("!: Your Ultra Sword is empty");
+                    }
+                } else {
+                    System.out.println("!: Player is not Doing Anything");
+                }
+                //AI
+                int bossfightexc = (int)(Math.random() * 4);
+                //Bossfight
+                if (bossfightexc == 0) {
+                    bossfight = "!: Bossfight is using Punch (4 Damage)";
+                    if (shieldPlayer < 0) {
+                        hpPlayer = hpPlayer - 4;
+                        shieldPlayer = 0;
+                    } else {
+                        shieldPlayer = shieldPlayer - 4;
+                        hpPlayer = hpPlayer - 2;
+                    }
+                } else if (bossfightexc == 1) {
+                    bossfight = "!: Bossfight is using Power Punch (6 Damage)";
+                    if (shieldPlayer < 0) {
+                        hpPlayer = hpPlayer - 6;
+                        shieldPlayer = 0;
+                    } else {
+                        shieldPlayer = shieldPlayer - 6;
+                        hpPlayer = hpPlayer - 4;
+                    }
+                } else if (bossfightexc == 2) {
+                    bossfight = "!: Bossfight is using Anime Punch (8 Damage)";
+                    if (shieldPlayer < 0) {
+                        hpPlayer = hpPlayer - 8;
+                        shieldPlayer = 0;
+                    } else {
+                        shieldPlayer = shieldPlayer - 8;
+                        hpPlayer = hpPlayer - 6;
+                    }
+                } else {
+                    bossfight = "!: Bossfight is not Doing Anything";
+                }
+                System.out.println(bossfight);
+            }
+        } else {
+            System.out.println("Well the Game Closed...\nCREATED BY MRCROCODILE");
+        }
+    }
+}
---
+++ BossfightII.java
@@ -0,0 +1,204 @@
+//Heya! MrCrocodile Dev Speaking
+//this is a long project so i just to make sure its not cringe game ._.
+import java.util.Scanner;
+public class BossfightII {
+    public static void main(String[] args) throws InterruptedException {
+        //User Input
+        Scanner user = new Scanner(System.in);
+        //Variables//
+        //Menu
+        int clickMe = 5;
+        int i = 0;
+        boolean folder;
+        //Game
+        boolean game;
+        int mv = 0;
+        boolean levelI = true;
+        boolean levelII = false;
+        boolean levelIII = false;
+        boolean levelIV = false;
+        boolean levelV = false;
+        String LvlI;
+        String LvlII;
+        String LvlIII;
+        String LvlIV;
+        String LvlV;
+        String xLvlI;
+        String xLvlII;
+        String xLvlIII;
+        String xLvlIV;
+        String xLvlV;
+        //Intro
+        System.out.println("MrCrocodile Presents :)");
+        Thread.sleep(2000);
+        System.out.println("Oops Somethinds Wrong....");
+        Thread.sleep(1000);
+        while (i < 5) {
+            System.out.println("Just enter " + clickMe + " times");
+            user.nextLine();
+            --clickMe;
+            ++i;
+            }
+        System.out.println("===WELCOME TO BOSSFIGHT II===\nType \"Start\", \"Guides\" & \"Folder\"\nCreated by MrCrocodile :D");
+        //CMD
+        while (true) {
+            String commandINP = user.nextLine();
+            if (commandINP.equals("Start")) {
+                //Start
+                System.out.println("Wanna Start the Game?\nType \"Yes\" to play a Bossfight II");
+                String showstart = user.nextLine();
+                if (showstart.equals("Yes")) {
+                    //Game
+                    game = true;
+                    if (game) {
+                        //If Level Locked
+                        if (levelI) {
+                            LvlI = "Unlocked";
+                            xLvlI = "1";
+                        } else {
+                            LvlI = "Locked";
+                            xLvlI = "X";
+                        }
+                        if (levelII) {
+                            LvlII = "Unlocked";
+                            xLvlII = "2";
+                        } else {
+                            LvlII = "Locked";
+                            xLvlII = "X";
+                        }
+                        if (levelIII) {
+                            LvlIII = "Unlocked";
+                            xLvlIII = "3";
+                        } else {
+                            LvlIII = "Locked";
+                            xLvlIII = "X";
+                        }
+                        if (levelIV) {
+                            LvlIV = "Unlocked";
+                            xLvlIV = "4";
+                        } else {
+                            LvlIV = "Locked";
+                            xLvlIV = "X";
+                        }
+                        if (levelV) {
+                            LvlV = "Unlocked";
+                            xLvlV = "5";
+                        } else {
+                            LvlV = "Locked";
+                            xLvlV = "X";
+                        }   
+                        //Map
+                        System.out.println("Welcome to\nBOSSFIGHT II\n---------------\n\n/||||||||\\\n               /||||||\\\n               ||||||||");
+                        System.out.println("(uwu)\n |       o     O    o\n |          o           /|||||||||||||||\\\n |\n | Slimes\n[" + xLvlI + "]————————["+ xLvlII + "]");
+                        while (mv < 4) {
+                            mv++;
+                            System.out.println("            |");
+                            if (mv == 4) {
+                                System.out.println("[" + xLvlIV + "]————————[" + xLvlIII + "]\n |             Giant Spider\n | \n |\n |\n[" + xLvlV + "]\n   Bossfight\n");
+                            } else if (mv == 1) {
+                                System.out.println("            |  Golems");
+                            } else if (mv == 3) {
+                                System.out.println(" Robot404   |");
+                            }
+                        }
+                        //Level
+                        Thread.sleep(2000);
+                        System.out.println("Level 1 " + LvlI);
+                        Thread.sleep(500);
+                        System.out.println("Level 2 " + LvlII);
+                        Thread.sleep(500);
+                        System.out.println("Level 3 " + LvlIII);
+                        Thread.sleep(500);
+                        System.out.println("Level 4 " + LvlIV);
+                        Thread.sleep(500);
+                        System.out.println("Level 5 " + LvlV);
+                        try {
+                            while (true) {
+                                int level = user.nextInt();
+                                switch (level) {
+                                case 1:
+                                if (levelI) {
+                                    System.out.println("bunch of Slimes can jump and punch you\n---------------\nRounds: 1-3\nDamages: 2-8hp\nEnemies: 3\nPress \"Start\" to start!");
+                                    String levelIStart = user.nextLine();
+                                    if (levelIStart.equals("Start")) {
+                                        //?COMMING SOON?//
+                                    } else {
+                                        System.out.println("Start Canceled...");
+                                    }
+                                } else {
+                                    System.out.println("Its Locked!");
+                                }
+                                break;
+                                case 2:
+                                if (levelII) {
+
+                                } else {
+                                    System.out.println("Its Locked!");
+                                }
+                                break;
+                                case 3:
+                                if (levelIII) {
+                                        
+                                } else {
+                                    System.out.println("Its Locked!");
+                                }
+                                break;
+                                case 4:
+                                if (levelIV) {
+                                        
+                                } else {
+                                    System.out.println("Its Locked!");
+                                }
+                                break;
+                                case 5:
+                                if (levelV) {
+                                        
+                                } else {
+                                    System.out.println("Its Locked!");
+                                }
+                                break;
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Type only the number!");
+                        }
+                    } else {
+                        System.out.println("Game Canceled...");
+                    }
+                } else if (commandINP.equals("Guides")) {
+                    //Guides
+                    System.out.println("===GUIDES in Bossfight II===\nwell please dont say cringe on me..\n----------\nSlimes can takes 2-8 Damages\nGolems can takes 4-10 Damages\nGiant Spider can takes 8-16 Damages\n404Robot can takes 10-16 Damages\nBossfight is unknowned...\n----------\n===You==\nHealth: 30\nShield: 40\n===Stats==\nRounds: 6\nAdditional Rounds: 3-6\n===Weapons===\nSwords, Rare Swords, Ultra Swords, Bows, Simple Bomb & Stength Potion");
+                } else if (commandINP.equals("Folder")) {
+                    //Folder
+                    folder = true;
+                    if (folder) {
+                        System.out.println("----------\n===FOLDER===\nSelect a Folder or File in your keyboard\nType \"Cancel\" to cancel folder\n\\--18+ <DIR>\n\\--credits.txt");
+                        while (true) {
+                            String chooseFol = user.nextLine();
+                            if (chooseFol.equals("18+")) {
+                                System.out.println("\\----18Plus.txt");
+                                String chooseFol2 = user.nextLine();
+                                if (chooseFol2.equals("18Plus.txt")) {
+                                System.out.println("Never gonna give you up\nnever gonna let you down");
+                                } else {
+                                    System.out.println("Folder not Exist");
+                                }
+                            } else if (chooseFol.equals("credits.txt")) {
+                                System.out.println("===CREDITS===\nPotato Chronicler\nNapstaparrot\nU-ndefined\nWELBIC\n===CREATED BY===\nMrCrocodile");
+                            } else if (chooseFol.equals("Cancel")) {
+                                folder = false;
+                                System.out.println("----------");
+                                break;
+                            } else {
+                                System.out.println("Folder not Exist");
+                            }
+                        }
+                    }
+                } else {
+                    //Not Exist
+                    System.out.println("\n\"" + commandINP + "\"" + "\n^ ERROR, not exist...");
+                }
+            }
+        }
+    }
+}
